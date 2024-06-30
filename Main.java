845dcf6a152fa224074072a5e7b6fad9303c84a6
afifbayu <129130395+afifbayu@users.noreply.github.com>
2024-06-30 16:06:50 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AFIF BAYU
+ */
+// Main Class
+public class Main {
+    public static void main(String[] args) {
+        MobilBaru mobilBaru = new MobilBaru("Toyota", "Avanza", "3 tahun");
+        MobilBekas mobilBekas = new MobilBekas("Honda", "Civic", 2018,200000000);
+    Dealer dealer = new Dealer("Mobilindo", mobilBaru, mobilBekas);
+    dealer.info();
+    }
+}
